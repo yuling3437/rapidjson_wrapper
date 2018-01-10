48ff72f5a5d3226a40caaf463c74ba8b0a8e8bae
yulf <343742895@qq.com>
2018-01-10 17:07:24 +0800

    readme

--- main.cpp
+++ main.cpp
@@ -2,54 +2,33 @@
 // Created by root on 18-1-8.
 //
 
-#include <stdio.h>
-#include "rapidjson/document.h"
-#include "rapidjson/stringbuffer.h"
-#include "rapidjson/prettywriter.h"
 #include <iostream>
-#include <Config.h>
 #include "JsonWrapper.h"
 
 using namespace std;
-using namespace rapidjson;
-Config* g_main_config;
-
-void print_json(Value &v)
-{
-    StringBuffer buffer;
-    Writer<StringBuffer> writer(buffer);
-
-    v.Accept(writer);
-    printf("%s\n", buffer.GetString());
-}
 
 int main()
 {
     JsonWrapper w, w1, w2;
     w.Prase("[1]");
-//    cout<<w[0].GetInt()<<endl;
+    cout<<w[0].GetInt()<<endl;
+
     w[1] = "2";
-    w["a"] = 1;
+    w["a"] = 1; //err
     cout<<"w:"<<w.ToString()<<endl;
 
     w1["a"] = 1;
     w1["b"] = 2;
 
-    w1["c"] = w;
+    w1["c"] = &w; //deep copy
+    w1["d"] = w;  //move
 
     cout<<"w:"<<w.ToString()<<endl;
 
     cout<<"w1:"<<w1.ToString()<<endl;
-//
-//
-//    Document doc(kArrayType);
-//
-//    doc.PushBack(Value(), doc.GetAllocator());
-//    doc[0].SetInt(2);
-//
-////    cout<<v[1].IsInt()<<endl;
-//
-//    print_json(doc);
+
+    w[1] = w1;
+    cout<<"w:"<<w.ToString()<<endl;
 
     return 0;
 }
